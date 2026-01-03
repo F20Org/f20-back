521d6aa57f1cd0126e7b770dd52cee5f0adf3911
PedroVidalDev <pedrohvidals@gmail.com>
2026-01-03 17:39:06 -0300

    feat: update dotenv dependency and configure environment variables loading in F20Application

--- src/main/java/com/pedro/f20/F20Application.java
+++ src/main/java/com/pedro/f20/F20Application.java
@@ -6,12 +6,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableScheduling
 public class F20Application {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
+        
+        dotenv.entries().forEach(entry -> 
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+
 		SpringApplication.run(F20Application.class, args);
 	}
 
